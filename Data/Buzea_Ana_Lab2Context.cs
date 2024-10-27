2ee99db88ba34986d5ac67c1b272f74aae2aadca
Radu <Radu@DESKTOP-1KGOH7J>
2024-10-27 19:11:09 +0200

    Add project files.

---
+++ Data/Buzea_Ana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Buzea_Ana_Lab2.Models;
+
+namespace Buzea_Ana_Lab2.Data
+{
+    public class Buzea_Ana_Lab2Context : DbContext
+    {
+        public Buzea_Ana_Lab2Context (DbContextOptions<Buzea_Ana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Buzea_Ana_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Buzea_Ana_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Buzea_Ana_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Buzea_Ana_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Navigational property
+        public ICollection<Book>? Books { get; set; }
+    }
+}
